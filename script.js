6b20af02bdebdd985cb99674525237d7728c368d
mahendrajat1996 <130475100+mahendrajat1996@users.noreply.github.com>
2023-07-03 22:16:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function delayHello() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+const outputElement = document.getElementById("output");
+
+delayHello()
+  .then((result) => {
+    outputElement.textContent = result;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
